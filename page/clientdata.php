9d0d7ae3b6a1f62f98fe0c33ae5c7f68ca788d07
marcel.arnold <marcel.arnold@BISONWS1253.infra.local>
2013-11-29 07:51:11 +0100

    correct behaviour if no data exists

--- page/clientdata.php
+++ page/clientdata.php
@@ -16,16 +16,20 @@
 
 	function processData(data) {
 		var res = '';
-		var lines = data.split("<?php echo Config::$LINE_SEPARATOR; ?>");
-		lines.forEach(function(entry) {
-		    var vals = entry.split("<?php echo Config::$DATA_SEPARATOR;?>");
-		    res += vals[0];
-		    res += " - ";
-		    res += vals[1];
-		    res += " - ";
-		    res += vals[2];
-		    res += "<br />";
-		})
+		if(data.length > 0) {
+			var lines = data.split("<?php echo Config::$LINE_SEPARATOR; ?>");
+			lines.forEach(function(entry) {
+			    var vals = entry.split("<?php echo Config::$DATA_SEPARATOR;?>");
+			    res += vals[0];
+			    res += " - ";
+			    res += vals[1];
+			    res += " - ";
+			    res += vals[2];
+			    res += "<br />";
+			})
+		} else {
+			res = 'no data';
+		}
 		return res;
 	}
 	//-->
